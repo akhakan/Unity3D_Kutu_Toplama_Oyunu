e378157d66aac6068ab2e4085326ebf566766eb4
akhakan <akhakana@hotmail.com>
2025-12-20 18:59:17 +0300

    code fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,34 +4,34 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private GameObject explosionPrefab;
 
-    [SerializeField] private Transform _orientationTransform; 
-    
-    private Rigidbody _playerRigidbody; 
-    
-    private float _verticalInput, _horizontalInput; 
-    
-    private Vector3 _movementDirection; 
-    
-    [SerializeField] private float _movementSpeed = 10f; 
+    [SerializeField] private Transform _orientationTransform;
+
+    private Rigidbody _playerRigidbody;
+
+    private float _verticalInput, _horizontalInput;
+
+    private Vector3 _movementDirection;
+
+    [SerializeField] private float _movementSpeed = 10f;
 
     private void Awake()
     {
-        _playerRigidbody = GetComponent<Rigidbody>(); 
-        _playerRigidbody.freezeRotation = true; 
+        _playerRigidbody = GetComponent<Rigidbody>();
+        _playerRigidbody.freezeRotation = true;
     }
 
     private void Update()
     {
         _verticalInput = Input.GetAxisRaw("Vertical");
         _horizontalInput = Input.GetAxisRaw("Horizontal");
-        _movementDirection = _orientationTransform.forward * _verticalInput + _orientationTransform.right * _horizontalInput;      
+        _movementDirection = _orientationTransform.forward * _verticalInput + _orientationTransform.right * _horizontalInput;
     }
 
 
     private void FixedUpdate()
     {
 
-        _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force); 
+        _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -42,25 +42,25 @@ public class PlayerController : MonoBehaviour
             if (box != null)
             {
                 ScoreManager.instance.AddScore(box.ScoreValue);
+                // Patlama efekti(isteðe baðlý)
+                Explode(explosionPrefab);
 
-                Renderer rend = box.GetComponent<Renderer>();
-                Color boxColor = rend.material.color;
-                boxColor *= Random.Range(0.85f, 1.15f);
-
-                GameObject explosion = Instantiate(explosionPrefab, box.transform.position, Quaternion.identity);
-
-                ParticleSystem ps = explosion.GetComponent<ParticleSystem>();
-                var main = ps.main;
-                main.startColor = boxColor;
-
-                // Instantiate(explosionPrefab, other.transform.position, Quaternion.identity);
-                
-                
-                
-                
                 Destroy(other.gameObject);
             }
         }
+    }
+
+    void Explode(GameObject other)
+    {
+        Renderer rend = other.GetComponent<Renderer>();
+        Color boxColor = rend.material.color;
+        boxColor *= Random.Range(0.85f, 1.15f);
+
+        GameObject explosion = Instantiate(explosionPrefab, other.transform.position, Quaternion.identity);
+
+        ParticleSystem ps = explosion.GetComponent<ParticleSystem>();
+        var main = ps.main;
+        main.startColor = boxColor;
     }
 
 }
